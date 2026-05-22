d618472113bdcc00385e06115baecd35df541f58
parth8454 <parth8454prajapati@gmail.com>
2026-05-22 18:31:14 +0530

    neuron layers and mlp

---
+++ engine/value.py
@@ -0,0 +1,61 @@
+class Value:
+  def __init__(self,data,_children=(),_op=''):
+    self.data = data
+    self.grad = 0.0
+    self.backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+
+  def __repr__(self):
+    return f"Value(data={self.data},grad={self.grad})"
+
+  def __add__(self,other):
+    other = other if isinstance(other,Value) else Value(other)
+    out = Value(self.data + other.data, (self,other), '+')
+
+    def _backward():
+      self.grad += 1.0 * out.grad
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
+    return out
+
+  def __mul__(self,other):
+    other = other if isinstance(other,Value) else Value(other)
+    out = Value(self.data * other.data,(self,other),'*')
+
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
+    return out
+  
+  def __pow__(self,other):
+    assert isinstance(other,(int,float))
+    out = Value(self.data**other,(self,other),'**')
+
+    def _backward():
+      self.grad+=(other * self.data**(other-1)) * out.grad
+    out._backward = _backward
+    return out
+  
+  def __neg__(self):
+    return self*-1
+  
+  def __sub__(self,other):
+    return self + (-other)
+
+  def backward(self):
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+      build_topo(self)
+
+      self.grad = 1.0
+
+      for node in reversed(topo):
+        node._backward()
---
+++ nn/model.py
@@ -0,0 +1,34 @@
+import random
+from engine.value import Value
+
+class Neuron:
+    def __init__(self,nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+
+    def __call__(self,x):
+        activation = sum((wi*xi for wi,xi in zip(self.w,x)),self.b) 
+        return activation   
+    
+class Layer:
+
+    def __init__(self,nin,nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self,x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+class MLP:
+
+    def __init__(self,nin,nouts): 
+        # nouts is a list of size of all layers
+
+        sz = [nin] + nouts
+
+        self.layers = [Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self,x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
